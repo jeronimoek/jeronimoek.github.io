2491a3a679fd6738b5f10406243a6e313582755b
jeronimoek <jeronimoek@gmail.com>
2021-10-14 03:53:54 -0300

    Fixed species

---
+++ species/build/precache-manifest.b4920f96c353eddb4e62e4acc6d02a45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c2ad32e006bf0f5d39745953ff3c1a9",
+    "url": "/species/build/index.html"
+  },
+  {
+    "revision": "097000edbea1212cb271",
+    "url": "/species/build/static/js/2.f238f960.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/species/build/static/js/2.f238f960.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "740f99e6510567e3326f",
+    "url": "/species/build/static/js/main.62650009.chunk.js"
+  },
+  {
+    "revision": "12c317efd4c20215eddc",
+    "url": "/species/build/static/js/runtime-main.703261d8.js"
+  }
+]);
