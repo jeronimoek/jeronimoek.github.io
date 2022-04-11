58d2f83980e495fbee7fae84c2fd07aae057b40e
jeronimoek <jeronimoek@gmail.com>
2022-04-11 10:02:08 -0300

    added facefilter test

--- testing-canvas/pages/collissionBalls/bouncyBalls.js
+++ testing-canvas/pages/collissionBalls/bouncyBalls.js
@@ -27,16 +27,20 @@ let ballsArray = [];
 const ballsSize = 15;
 const explosionPng = new Image();
 let hasExplosions = true;
+let hasCollisions = false;
+let hasGravity = true;
+let collisionedInLastFrame = {};
 
 function drawImage() {
   class Ball {
-    constructor(x, y, color, velocityX) {
+    constructor(id, x, y, color, velocityX) {
+      this.id = id;
       this.x = x;
       this.y = y;
       this.color = color;
       this.size = ballsSize;
       this.velocityX = velocityX;
-      this.velocityY = 0;
+      this.velocityY = hasGravity ? 0 : Math.random() * 20;
     }
     draw() {
       ctx.beginPath();
@@ -47,11 +51,11 @@ function drawImage() {
     update() {
       ctx.fillStyle = this.color;
 
-      // collission detection with borders
+      // collision detection with borders
       if (this.y + this.size > window.innerHeight && this.velocityY >= 0) {
         // console.log("here2");
         this.y = window.innerHeight - this.size + 1;
-        this.velocityY = -this.velocityY + 1;
+        this.velocityY = -this.velocityY + (hasGravity ? 1 : 0);
       } else if (this.y - this.size < 0) {
         this.y = 0 + this.size;
         this.velocityY = -this.velocityY;
@@ -61,7 +65,9 @@ function drawImage() {
           this.y + this.size > window.innerHeight + 2
         )
       ) {
-        this.velocityY += 1;
+        if (hasGravity) {
+          this.velocityY += 1;
+        }
       }
       if (this.x + this.size > window.innerWidth) {
         // console.log("here1");
@@ -83,6 +89,7 @@ function drawImage() {
       let color = `hsl(${Math.round(Math.random() * 360)}, 100%, 50%)`;
       ballsArray.push(
         new Ball(
+          i,
           ballsSize + Math.random() * (window.innerWidth - ballsSize * 2),
           ballsSize + Math.random() * (window.innerHeight - ballsSize * 2),
           color,
@@ -100,10 +107,9 @@ function drawImage() {
       explosionPng.height / 4
     );
   }
-  function checkBallsCollission() {
+  function checkBallsCollision() {
     for (let i = 0; i < ballsArray.length; i++) {
       for (let j = i + 1; j < ballsArray.length; j++) {
-        console.log(i, j);
         const b1 = ballsArray[i];
         const b2 = ballsArray[j];
         // TODO: puede que esto este mal y haya que cambiar de lado b1 b2 --->
@@ -121,79 +127,99 @@ function drawImage() {
           // });
 
           // Calculo el pase de velocidades entre las bolas
-          const b1VVal = (b1.velocityX ** 2 + b1.velocityY ** 2) ** 0.5;
-          const b1VAng = Math.atan2(b1.velocityY, b1.velocityX);
-          const b2VVal = (b2.velocityX ** 2 + b2.velocityY ** 2) ** 0.5;
-          const b2VAng = Math.atan2(b2.velocityY, b2.velocityX);
-          const normAng = Math.atan2(c, a);
-          const b1VToNormAng = b1VAng - normAng;
-          const b1VNormVal = b1VVal * Math.cos(b1VToNormAng);
-          const b1VTangVal = b1VVal * Math.sin(b1VToNormAng);
-          const b2VToNormAng = b2VAng - normAng;
-          const b2VNormVal = b2VVal * Math.cos(b2VToNormAng);
-          const b2VTangVal = b2VVal * Math.sin(b2VToNormAng);
-          const b1VFinalVal = (b1VTangVal ** 2 + b2VNormVal ** 2) ** 0.5;
-          const b1VFinalAngToX = Math.atan2(b1VTangVal, b2VNormVal) + normAng;
-          const b2VFinalVal = (b2VTangVal ** 2 + b1VNormVal ** 2) ** 0.5;
-          const b2VFinalAngToX = Math.atan2(b2VTangVal, b1VNormVal) + normAng;
-          const b1VFinalValX = b1VFinalVal * Math.cos(b1VFinalAngToX);
-          const b1VFinalValY = b1VFinalVal * Math.sin(b1VFinalAngToX);
-          const b2VFinalValX = b2VFinalVal * Math.cos(b2VFinalAngToX);
-          const b2VFinalValY = b2VFinalVal * Math.sin(b2VFinalAngToX);
-          b1.velocityX = b1VFinalValX;
-          b1.velocityY = b1VFinalValY;
-          b2.velocityX = b2VFinalValX;
-          b2.velocityY = b2VFinalValY;
+          if (hasCollisions) {
+            console.log(
+              collisionedInLastFrame,
+              !!(
+                (collisionedInLastFrame[b1.id] &&
+                  collisionedInLastFrame[b1.id].indexOf(b2.id) > -1) ||
+                (collisionedInLastFrame[b2.id] &&
+                  collisionedInLastFrame[b2.id].indexOf(b1.id) > -1)
+              )
+            );
+            if (
+              (collisionedInLastFrame[b1.id] &&
+                collisionedInLastFrame[b1.id].indexOf(b2.id) > -1) ||
+              (collisionedInLastFrame[b2.id] &&
+                collisionedInLastFrame[b2.id].indexOf(b1.id) > -1)
+            ) {
+              return;
+            }
+            const b1VVal = (b1.velocityX ** 2 + b1.velocityY ** 2) ** 0.5;
+            const b1VAng = Math.atan2(b1.velocityY, b1.velocityX);
+            const b2VVal = (b2.velocityX ** 2 + b2.velocityY ** 2) ** 0.5;
+            const b2VAng = Math.atan2(b2.velocityY, b2.velocityX);
+            const normAng = Math.atan2(c, a);
+            const b1VToNormAng = b1VAng - normAng;
+            const b1VNormVal = b1VVal * Math.cos(b1VToNormAng);
+            const b2VToNormAng = b2VAng - normAng;
+            const b2VNormVal = b2VVal * Math.cos(b2VToNormAng);
+            const b1VTangVal = b1VVal * Math.sin(b1VToNormAng);
+            const b2VTangVal = b2VVal * Math.sin(b2VToNormAng);
+            const b1VFinalVal = (b1VTangVal ** 2 + b2VNormVal ** 2) ** 0.5;
+            const b1VFinalAngToX = Math.atan2(b1VTangVal, b2VNormVal) + normAng;
+            const b2VFinalVal = (b2VTangVal ** 2 + b1VNormVal ** 2) ** 0.5;
+            const b2VFinalAngToX = Math.atan2(b2VTangVal, b1VNormVal) + normAng;
+            const b1VFinalValX = b1VFinalVal * Math.cos(b1VFinalAngToX);
+            const b1VFinalValY = b1VFinalVal * Math.sin(b1VFinalAngToX);
+            const b2VFinalValX = b2VFinalVal * Math.cos(b2VFinalAngToX);
+            const b2VFinalValY = b2VFinalVal * Math.sin(b2VFinalAngToX);
+            b1.velocityX = b1VFinalValX;
+            b1.velocityY = b1VFinalValY;
+            b2.velocityX = b2VFinalValX;
+            b2.velocityY = b2VFinalValY;
+            // Evito el bug de cuando se solapan continuamente
+            // Las muevo en la direccion de la reacción hasta sus limites.
+            const ammountOfSeparation = ballsSize * 2 - bSq ** 0.5;
+            const separationX = ammountOfSeparation * Math.cos(normAng);
+            const separationY = ammountOfSeparation * Math.sin(normAng);
+            b1.x += separationX;
+            b1.y += separationY;
+            b2.x += separationX;
+            b2.y += separationY;
+            if (collisionedInLastFrame[b1.id]) {
+              collisionedInLastFrame[b1.id].push(b2.id);
+            } else {
+              collisionedInLastFrame[b1.id] = [b2.id];
+            }
 
-          // Evito el bug de cuando se solapan continuamente
-          // Las muevo en la direccion de la reacción hasta sus limites.
-          const ammountOfSeparation = ballsSize * 2 - bSq ** 0.5;
-          const separationX = ammountOfSeparation * Math.cos(normAng);
-          const separationY = ammountOfSeparation * Math.sin(normAng);
-          b1.x += separationX;
-          b1.y += separationY;
-          b2.x += separationX;
-          b2.y += separationY;
-          ctx.strokeStyle = "white";
-          ctx.beginPath(); // Start a new path
-          ctx.moveTo(b1.x, b1.y); // Move the pen to (30, 50)
-          ctx.lineTo(b1.x + b1.velocityX * 5, b1.y + b1.velocityY * 5); // Draw a line to (150, 100)
-          ctx.lineWidth = 15;
-          ctx.stroke();
-          ctx.strokeStyle = "red";
-          ctx.beginPath(); // Start a new path
-          ctx.moveTo(b2.x, b2.y); // Move the pen to (30, 50)
-          ctx.lineTo(b2.x + b2.velocityX * 5, b2.y + b2.velocityY * 5); // Draw a line to (150, 100)
-          ctx.lineWidth = 15;
-          ctx.stroke();
-          // setTimeout(alert, 1);
-
-          // console.log({
-          //   verify: {
-          //     x: b1.velocityX + b2.velocityX,
-          //     y: b1.velocityY + b2.velocityY,
-          //   },
-          // });
-          // explosion(b1.x, b1.y);
-          // playSound(0.2);
-          // ballsArray = ballsArray.filter(
-          //   (_v, index) => index !== i && index !== j
-          // );
+            // ctx.strokeStyle = "white";
+            // ctx.beginPath(); // Start a new path
+            // ctx.moveTo(b1.x, b1.y); // Move the pen to (30, 50)
+            // ctx.lineTo(b1.x + b1.velocityX * 5, b1.y + b1.velocityY * 5); // Draw a line to (150, 100)
+            // ctx.lineWidth = 15;
+            // ctx.stroke();
+            // ctx.strokeStyle = "red";
+            // ctx.beginPath(); // Start a new path
+            // ctx.moveTo(b2.x, b2.y); // Move the pen to (30, 50)
+            // ctx.lineTo(b2.x + b2.velocityX * 5, b2.y + b2.velocityY * 5); // Draw a line to (150, 100)
+            // ctx.lineWidth = 15;
+            // ctx.stroke();
+            // setTimeout(alert, 1);
+          }
+          if (hasExplosions) {
+            explosion(b1.x, b1.y);
+            playSound(0.2);
+            ballsArray = ballsArray.filter(
+              (_v, index) => index !== i && index !== j
+            );
+          }
         }
       }
     }
+    collisionedInLastFrame = {};
   }
   let animatedRunningCurrent = 0;
   function animate(animatedRunning) {
     if (animatedRunning === animatedRunningCurrent) {
       requestAnimationFrame(() => animate(animatedRunning));
-      ctx.fillStyle = "rgba(0,0,0,0.1)";
+      ctx.fillStyle = "rgba(0,0,0,1)";
       ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
       for (let i = 0; i < ballsArray.length; i++) {
         ballsArray[i].update();
         ballsArray[i].draw();
       }
-      if (hasExplosions) checkBallsCollission();
+      if (hasExplosions || hasCollisions) checkBallsCollision();
     }
   }
   init();
@@ -217,12 +243,16 @@ function drawImage() {
     }
     ballsQuantity = quantity;
     hasExplosions = document.getElementById("Explosions").checked;
+    hasCollisions = document.getElementById("Collisions").checked;
+    hasGravity = document.getElementById("Gravity").checked;
     restart();
   });
 
   document.getElementById("Cancel").addEventListener("click", () => {
     document.getElementById("Quantity").value = ballsQuantity;
     document.getElementById("Explosions").checked = hasExplosions;
+    document.getElementById("Collisions").checked = hasCollisions;
+    document.getElementById("Gravity").checked = hasGravity;
   });
 }
 
