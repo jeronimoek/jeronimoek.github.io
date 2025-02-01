87d9668d17389f8cce537293e6718cc2f9ea1c45
jeronimoek <jeronimoek@gmail.com>
2025-02-01 20:01:25 +0100

    feat: update limpieza web

--- limpieza/index.js
+++ limpieza/index.js
@@ -1,5 +1,11 @@
-const people = ['Jero','Senen','Manu','Miguel']
-
+const people = [
+    {name: 'Hab. 4',color: 'red'},
+    {name:'Hab. 2',color: 'green'},
+    {name:'Hab. 3',color: 'blue'},
+    {name:'Hab. 1',color: 'black'},]
+const tasks = [
+    'COCINA','LIVING','BAÑO','REEMPLAZO'
+]
 
 
 const now = new Date()
@@ -10,16 +16,25 @@ for(let i = 0; i < 20; i++){
     now.setDate(now.getDate() + 6)
     const lastDay = `${now.getDate()}/${now.getMonth()+1}`
     const p = document.createElement('p')
+    if(i === 2){
+        p.style.fontSize = '30px'
+    }
     p.innerText= firstDay + ' - ' + lastDay
     p.style.margin = '0px'
     document.querySelector('#container').appendChild(p)
 
     const weekShift = Math.floor(now.getTime()/(86400 * 1000 * 7))%4
 
-    const arr = people.slice(weekShift).concat(people.slice(0,weekShift))
+    const habs = people.slice(weekShift).concat(people.slice(0,weekShift))
 
     const s = document.createElement('p')
-    s.innerText = `cocina: ${arr[0]}\nliving: ${arr[1]}\nbaño: ${arr[2]}\nreemplazo: ${arr[3]}`
+    habs.forEach((hab, i) => {
+        const habElem = document.createElement('div')
+        habElem.style.color = hab.color
+        habElem.innerText = `${tasks[i]}: ${hab.name}`
+        s.appendChild(habElem)
+    })
+    // s.innerText = `cocina: ${arr[0]}\nliving: ${arr[1]}\nbaño: ${arr[2]}\nreemplazo: ${arr[3]}`
     document.querySelector('#container').appendChild(s)
     s.style.margin = '0px'
     s.style.marginBottom = '20px'
